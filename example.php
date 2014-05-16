4c7b14203b2def8ea861e04e1cd066bf0aa44c95
denheck <denheck@gmail.com>
2014-05-16 11:44:52 -0500

    Update example.php

--- example.php
+++ example.php
@@ -4,3 +4,17 @@ $crontab->at("7pm")->on("tuesdays")->do("/bin/false");
 $crontab->each('month')->on('first day')->do('/bin/false');
 $crontab->at('7pm')->do('/bin/false')->withEnvironment(['APPLICATION_ENV' => 'production', 'DOMAIN' => 'www.offers.com']);
 $crontab->withEnvironment('APPLICATION_ENV', 'production');
+
+$crontab->on('reboot')->do('/bin/task');
+
+$crontab->at("17:00")->on('wednesdays')->do(function () {
+    $command = "echo test";
+    return $command;
+});
+
+// escape command line arguments 
+$userInput = "escaped user 'input'";
+$crontab->every('day')->do("echo", $userInput);
+
+// get list of jobs
+$crontab->jobs();
