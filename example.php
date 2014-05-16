6929f3a4fc7837e4040ec10868fbaea9d5cdf4b4
Josh Butts <josh@joshbutts.com>
2014-05-16 16:02:35 -0500

    add files

--- example.php
+++ example.php
@@ -1,20 +1,25 @@
 <?php
 $crontab->every("30 minutes")->do("/bin/whoami");
-$crontab->at("7pm")->on("tuesdays")->do("/bin/false");
-$crontab->each('month')->on('first day')->do('/bin/false');
+
+$crontab->at("7pm")->every("tuesday")->do("/bin/false");
+
+$crontab->every('1st')->do('/bin/false');
+
+$crontab->every('monday')->do('/bin/false')->as('root');
+
 $crontab->at('7pm')->do('/bin/false')->withEnvironment(['APPLICATION_ENV' => 'production', 'DOMAIN' => 'www.offers.com']);
+
 $crontab->withEnvironment('APPLICATION_ENV', 'production');
 
-$crontab->on('reboot')->do('/bin/task');
+$crontab->every('reboot')->do('/bin/task');
 
-$crontab->at("17:00")->on('wednesdays')->do(function () {
-    $command = "echo test";
-    return $command;
+$crontab->withEnvironment('APPLICATION_ENV', 'production');
+
+$crontab->every('minute')->do('/bin/whoami')->as('root');
+
+$crontab->cron('0 * * * *')->do('/bin/whoami');
+
+$crontab->every('monday')->do(function() {
+    $this->do('/bin/false');
+    $this->do('/bin/false');
 });
-
-// escape command line arguments 
-$userInput = "escaped user 'input'";
-$crontab->every('day')->do("echo", $userInput);
-
-// get list of jobs
-$crontab->jobs();
---
+++ src/Punctual/Crontab.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josh
+ * Date: 5/16/14
+ * Time: 3:53 PM
+ */ 
